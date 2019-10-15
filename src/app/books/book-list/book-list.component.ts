d728664d0df33d50b84148d3e10cf34c6e25d71a
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-10-15 16:05:27 +0200

    http

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BookDataService } from '../book-data.service';
 import { Book } from '../book';
+import { switchMap, map, take } from 'rxjs/operators';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'me-book-list',
@@ -13,6 +15,16 @@ export class BookListComponent implements OnInit {
   constructor(private bookData: BookDataService) {}
 
   ngOnInit() {
-    this.bookData.getBooks().subscribe(books => (this.books = books));
+    this.bookData
+      .getBooks()
+      .pipe(
+        switchMap((books: Book[]) =>
+          interval(1000).pipe(
+            map(i => books.slice(0, i + 1)),
+            take(books.length)
+          )
+        )
+      )
+      .subscribe(books => (this.books = books));
   }
 }
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BookListComponent } from './book-list/book-list.component';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books/books.component';
-import { BookListComponent } from './book-list/book-list.component';
-
 
 @NgModule({
   declarations: [BooksComponent, BookListComponent],
-  imports: [
-    CommonModule,
-    BooksRoutingModule
-  ],
+  imports: [CommonModule, BooksRoutingModule, HttpClientModule],
   exports: [BooksComponent, BookListComponent]
 })
-export class BooksModule { }
+export class BooksModule {}
