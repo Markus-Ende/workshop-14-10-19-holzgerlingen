2072ca6f12eb3276eb57adfeb2880e97cca441e0
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-10-16 10:50:10 +0200

    basic routing

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'me-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
+export class AboutComponent implements OnInit, OnDestroy {
+  constructor() {}
 
   ngOnInit() {
+    // interval(1000).subscribe(i => console.log(i));
+    console.log('about init');
   }
-
+  ngOnDestroy(): void {
+    console.log('about destroy');
+  }
 }
